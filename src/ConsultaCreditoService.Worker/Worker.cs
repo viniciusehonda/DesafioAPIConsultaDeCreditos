a4c197932ad53a96df9793ae4a56bbfb0ee3ad6f
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-19 15:02:51 -0300

    fix: corrigio pacotes ConsultaCreditoService.Worker

--- src/ConsultaCreditoService.Worker/Worker.cs
+++ src/ConsultaCreditoService.Worker/Worker.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Hosting;
+
 namespace ConsultaCreditoService.Worker;
 
 public class Worker : BackgroundService
@@ -15,7 +17,7 @@ public class Worker : BackgroundService
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
             }
             await Task.Delay(1000, stoppingToken);
         }
