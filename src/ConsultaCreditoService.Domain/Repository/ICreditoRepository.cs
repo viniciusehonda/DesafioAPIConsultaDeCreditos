8ac2cf7bd295d6103869e88dfe0964fd5ee9cb45
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-18 21:42:11 -0300

    feat: interface para o repositório para o objeto Credito

---
+++ src/ConsultaCreditoService.Domain/Repository/ICreditoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCreditoService.Domain.Entities;
+
+namespace ConsultaCreditoService.Domain.Repository;
+public interface ICreditoRepository
+{
+    Task AddCredito(Credito credito);
+    Task<List<Credito>> GetCreditosByNfse(string nfse);
+    Task<List<Credito>> GetCreditosByNumeroCredito(string numeroCredito); 
+}
