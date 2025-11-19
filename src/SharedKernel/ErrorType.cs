f6836e5b0da20262f66de30c04389606d537eb36
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-19 17:55:19 -0300

    feat: adicionado projeto SharedKernel com implementação para padrão Result

---
+++ src/SharedKernel/ErrorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel;
+public enum ErrorType
+{
+    Failure = 0,
+    Validation = 1,
+    Problem = 2,
+    NotFound = 3,
+    Conflict = 4
+}
