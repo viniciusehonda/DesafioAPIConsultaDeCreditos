d4e9ce9289c70bf28814f87db8c6ef90dd82b76d
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-19 00:02:04 -0300

    feat: adicionado injeção de dependência do projeto Infrastructure

--- src/ConsultaCreditoService.Api/Program.cs
+++ src/ConsultaCreditoService.Api/Program.cs
@@ -1,6 +1,10 @@
+using ConsultaCreditoService.Infrastructure;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/ConsultaCreditoService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsultaCreditoService.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsultaCreditoService.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+         services.AddServices()
+            .AddDatabase(configuration);
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ConsultaCreditoServiceDbContext>(
+            options => options
+                .UseNpgsql(connectionString, npgsqlOptions =>
+                    npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Default)));
+
+        return services;
+    }
+}
