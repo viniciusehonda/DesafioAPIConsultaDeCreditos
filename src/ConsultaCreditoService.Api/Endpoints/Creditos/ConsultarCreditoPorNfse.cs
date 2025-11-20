af90514e241d6c5f73d2ff7d6008a5e05a7cfaac
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-20 18:51:16 -0300

    feat: adicionando endpoints para consultar creditos por nfse

---
+++ src/ConsultaCreditoService.Api/Endpoints/Creditos/ConsultarCreditoPorNfse.cs
@@ -0,0 +1,25 @@
+﻿
+using ConsultaCreditoService.Api.Extensions;
+using ConsultaCreditoService.Api.Infrastructure;
+using ConsultaCreditoService.Application.Abstractions.Messaging;
+using ConsultaCreditoService.Application.Credito.ConsultarCreditoPorNfse;
+using SharedKernel;
+
+namespace ConsultaCreditoService.Api.Endpoints.Creditos;
+
+internal sealed class ConsultarCreditoPorNfse : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/creditos/{numeroNfse}", async (
+            string numeroNfse,
+            IQueryHandler<ConsultarCreditoPorNfseQuery, List<ConsultarCreditoPorNfseResponse>> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new ConsultarCreditoPorNfseQuery(numeroNfse);
+            Result<List<ConsultarCreditoPorNfseResponse>> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        }).WithTags("Creditos");
+    }
+}
