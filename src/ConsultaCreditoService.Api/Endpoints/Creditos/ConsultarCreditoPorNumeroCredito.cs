70188d2035f98cf4f2994f2ae1430d50d033fc34
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2025-11-20 18:51:35 -0300

    feat: adicionado endpoint para consultar credito por numero do credito

---
+++ src/ConsultaCreditoService.Api/Endpoints/Creditos/ConsultarCreditoPorNumeroCredito.cs
@@ -0,0 +1,26 @@
+﻿
+using ConsultaCreditoService.Api.Extensions;
+using ConsultaCreditoService.Api.Infrastructure;
+using ConsultaCreditoService.Application.Abstractions.Messaging;
+using ConsultaCreditoService.Application.Credito.ConsultarCreditoPorNfse;
+using ConsultaCreditoService.Application.Credito.ConsultarCreditoPorNumeroCredito;
+using SharedKernel;
+
+namespace ConsultaCreditoService.Api.Endpoints.Creditos;
+
+internal sealed class ConsultarCreditoPorNumeroCredito : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/creditos/credito/{numeroCredito}", async (
+            string numeroCredito,
+            IQueryHandler<ConsultarCreditoPorNumeroCreditoQuery, ConsultarCreditoPorNumeroCreditoResponse> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new ConsultarCreditoPorNumeroCreditoQuery(numeroCredito);
+            Result<ConsultarCreditoPorNumeroCreditoResponse> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        }).WithTags("Creditos");
+    }
+}
